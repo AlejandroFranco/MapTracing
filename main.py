1e80eb709064f92e207307f0942afa4ed9897665
Pablo Alejandro Franco Lemus <16484144+AlejandroFranco@users.noreply.github.com>
2020-10-06 21:38:18 -0600

    Trabajando en el automada finito determinista

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 class Trayectoria:
-    #una trayectoria tiene un conjunto de estaciones, rutas y un peso del trayecto
-    def _init_(self, estaciones, rutas, pesotry):
+    # una trayectoria tiene un conjunto de estaciones, rutas y un peso del trayecto
+    def _init_(self, estaciones, rutas, pesotrayecto):
         self.estaciones = estaciones
         self.rutas = rutas
-        self.pesotry = pesotry
+        self.pesotrayecto = pesotrayecto
+
 
 class Ruta:
     def _init_(self, nombre, peso, einicio, efin):
@@ -11,6 +12,7 @@ class Ruta:
         self.peso = peso
         self.einicio = einicio
         self.egin = efin
+
 
 class Estacion:
     def _init_(self, nombre, estado, color):
@@ -20,23 +22,44 @@ class Estacion:
 
 
 class Lector:
+
+    def decor(self, func):
+        def wrap():
+            print("\nPABLO ALEJANDRO FRANCO LEMUS\nLenguajes Formales y de programación\n")
+            print("\n201708993\n")
+
+        return wrap()
+
+    @decor
     def menu(self):
-        print("\nPABLO ALEJANDRO FRANCO LEMUS\nLenguajes Formales y de programación\n")
-        print("\n201708993\n")
         print("1. Cargar Archivo")
         print("2. Graficar Ruta")
         print("3. Graficar Mapa")
         print("3. salir")
         opcion = input(">>Ingrese una opción: ")
         if opcion == "1":
-            cargarArchivo()
+            analizarArchv(cargarArchivo())
+            self.menu()
         elif opcion == "2":
             print("2")
         elif opcion == "3":
             quit()
         else:
             print("Ingrese un numero 1-3")
-            menu()
+            self.menu()
+
+
+def analizarArchv(contenido):
+    rutas = []
+    estaciones = []
+    estado = 0
+    lexema = ''
+    for linea in contenido:
+        for caracter in linea:
+            if caracter == '\n':
+                continue
+            lexema = lexema + caracter
+
 
 def cargarArchivo():
     ruta = input(">>Escriba la ruta completa del archivo: ")
@@ -46,6 +69,7 @@ def cargarArchivo():
             archivo = open(ruta, "r")
             contenido = archivo.read()
             archivo.close()
+            return contenido
         except FileNotFoundError:
             print("Archivo no encontrado...")
             print("\n")
@@ -54,3 +78,6 @@ def cargarArchivo():
         print("Ingrese una ruta y extensión correcta (.txt) para el archivo")
         cargarArchivo()
 
+
+lector = Lector()
+lector.menu()
