3cfe71f9f6530f8b5b49a0c29399708a69d159f2
Pablo Alejandro Franco Lemus <16484144+AlejandroFranco@users.noreply.github.com>
2020-10-07 19:01:02 -0600

    Trabajando en el automata finito determinista

--- main.py
+++ main.py
@@ -22,23 +22,22 @@ class Estacion:
 
 
 class Lector:
-
     def decor(self, func):
         def wrap():
             print("\nPABLO ALEJANDRO FRANCO LEMUS\nLenguajes Formales y de programación\n")
             print("\n201708993\n ")
-
+            func()
         return wrap()
 
-    @decor
+
     def menu(self):
         print("1. Cargar Archivo")
         print("2. Graficar Ruta")
         print("3. Graficar Mapa")
-        print("3. salir")
+        print("3. Salir")
         opcion = input(">>Ingrese una opción: ")
         if opcion == "1":
-            analizarArchv(cargarArchivo())
+            analisisLexico(cargarArchivo())
             self.menu()
         elif opcion == "2":
             print("2")
@@ -49,7 +48,7 @@ class Lector:
             self.menu()
 
 
-def analizarArchv(contenido):
+def analisisLexico(contenido):
     rutas = []
     estaciones = []
     estado = 0
@@ -58,7 +57,33 @@ def analizarArchv(contenido):
         for caracter in linea:
             if caracter == '\n':
                 continue
+
             lexema = lexema + caracter
+
+            if estado == 0:
+                if caracter == "<":
+                    estado = 1
+            elif estado == 1:
+                if 65 <= ord(caracter) <= 122:
+                    estado = 5
+                elif caracter == '/':
+                    estado = 2
+                else:
+                    estado = -1
+            elif estado == 2:
+                if 65 <= ord(caracter) <= 122:
+                    estado = 3
+            elif estado == -1:
+                print('Token incorrecto', lexema)
+            elif estado == 3:
+                if caracter == '>':
+                    estado = 4
+        if estado == 4:
+            print(lexema, 'aceptado\n')
+        else:
+            print('cadena: ', lexema, 'inválido\n')
+        estado = 0
+        lexema = ''
 
 
 def cargarArchivo():
